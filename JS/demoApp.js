76f6abbfed7df5e1dae88aafbf45eec6a43ecb36
adevelopz <adev4psn@gmail.com>
2016-02-25 16:44:49 +0700

    ..

--- JS/demoApp.js
+++ JS/demoApp.js
@@ -1,5 +1,7 @@
-    angular.module("myapp", [])
+(function(angular) {
+	angular.module("myapp.HelloController", [])
     .controller("HelloController", function($scope) {
     $scope.helloTo = {};
     $scope.helloTo.title = "World, AngularJS";
     });
+})(angular);
